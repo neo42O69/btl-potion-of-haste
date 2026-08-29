0822465215a8f0a87ad5e1dd57d6f60c929b6a65
battlestickk <geniykut10@gmail.com>
2026-08-29 17:54:36 +0200

    initial commit failed so im committing again

---
+++ src/main/java/com/battlestickk/potionofhaste/BtlPotionOfHaste.java
@@ -0,0 +1,26 @@
+package com.battlestickk.potionofhaste;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BtlPotionOfHaste implements ModInitializer
+	{
+	public static final String MOD_ID = "btl-potion-of-haste";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+
+	public static Identifier id(String path)
+	{
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
